dc247a5bcc87d383252bff7e1cb1a20f4d0ef8d9
Igor Tyulyakov <g101k@mail.ru>
2024-01-06 10:55:04 +0300

    27 parameterize card span (#30)
    * Refactoring
    
    * Cant parameterize
    
    * Update to 17ver

--- Oip/ClientApp/projects/oip/src/app/app.routes.ts
+++ Oip/ClientApp/projects/oip/src/app/app.routes.ts
@@ -1,14 +1,18 @@
 import { Routes } from '@angular/router';
-import { HomeComponent } from './home/home.component';
 import { NotfoundComponent } from './demo/components/notfound/notfound.component';
 import { ModuleConfigComponent } from "./config/module-config.component";
-import { AppLayoutComponent } from "./layout/app.layout.component";
-import { DashboardComponent } from "./demo/components/dashboard/dashboard.component";
 
 export const APP_ROUTES: Routes = [
   {
-    path: '', loadChildren: () => import('./demo/components/dashboard/dashboard.module').then(m => m.DashboardModule)
+    path: '',
+    loadChildren: () => import('./demo/components/dashboard/dashboard.module').then(m => m.DashboardModule)
   },
+  {
+    path: 'block-marketing',
+    loadChildren: () => import('./demo/components/primeblocks/primeblocks.module').then(m => m.PrimeBlocksModule)
+  },
+
+
   {
     path: 'config',
     component: ModuleConfigComponent,
