c2dd2a230f0cf338c85c84a50b0615633c9617cf
Igor Tyulyakov <g101k@mail.ru>
2025-01-04 16:10:16 +0300

    Create BaseFeatureController.cs

--- src/Oip/Controllers/WeatherForecastController.cs
+++ src/Oip/Controllers/WeatherForecastController.cs
@@ -1,7 +1,6 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Oip.Controllers.Api;
-using Oip.Data.Dtos;
 using Oip.Data.Repositories;
 
 #pragma warning disable CS1591
@@ -10,33 +9,19 @@ namespace Oip.Controllers;
 
 [ApiController]
 [Route("api/[controller]")]
-public class WeatherForecastController : ControllerBase
+public class WeatherForecastController : BaseFeatureController
 {
-    private readonly FeatureRepository _featureRepository;
-
-    private readonly List<SecurityResponse> _defaultFeatureSecurity = new()
-    {
-        new() { Code = "read", Name = "Read", Description = "Can view this feature", Roles = ["admin"] },
-        new() { Code = "edit", Name = "Edit", Description = "Can edit data", Roles = ["admin"] },
-        new() { Code = "delete", Name = "Delete", Description = "Can delete edit data", Roles = ["admin"] },
-    };
-
     private static readonly string[] Summaries =
     {
         "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
     };
-#pragma warning disable S4487
-    private readonly ILogger<WeatherForecastController> _logger;
-#pragma warning restore S4487
+
     /// <summary>
     /// .ctor
     /// </summary>
-    /// <param name="logger"></param>
     /// <param name="featureRepository"></param>
-    public WeatherForecastController(ILogger<WeatherForecastController> logger, FeatureRepository featureRepository)
+    public WeatherForecastController(FeatureRepository featureRepository) : base(featureRepository)
     {
-        _logger = logger;
-        _featureRepository = featureRepository;
     }
 
     [HttpGet]
@@ -52,52 +37,13 @@ public class WeatherForecastController : ControllerBase
             .ToArray());
     }
 
-    /// <summary>
-    /// Get security for instance id
-    /// </summary>
-    /// <param name="id"></param>
-    /// <returns></returns>
-    [HttpGet]
-    [Authorize(Roles = "admin")]
-    [Route("get-security")]
-    public async Task<List<SecurityResponse>> GetSecurity(int id)
+    public override List<SecurityResponse> GetFeatureRights()
     {
-        var roleRightPair = await _featureRepository.GetSecurityByInstanceId(id);
-        var result = new List<SecurityResponse>();
-        foreach (var security in _defaultFeatureSecurity)
+        return new()
         {
-            security.Roles = roleRightPair.Where(x => x.Right == security.Code).Select(x => x.Role).Distinct().ToList();
-            result.Add(security);
-        }
-
-        return result;
-    }
-
-    /// <summary>
-    /// Update security
-    /// </summary>
-    /// <param name="request"></param>
-    /// <returns></returns>
-    [HttpPut]
-    [Authorize(Roles = "admin")]
-    [Route("put-security")]
-    public async Task<IActionResult> PutSecurity(PutSecurityRequest request)
-    {
-        List<FeatureSecurityDto> featureSecurityDtos = new();
-        foreach (var security in request.Securities)
-        {
-            if (security.Roles is null) continue;
-            foreach (var role in security.Roles)
-            {
-                featureSecurityDtos.Add(new FeatureSecurityDto()
-                {
-                    Right = security.Code,
-                    Role = role
-                });
-            }
-        }
-
-        await _featureRepository.UpdateInstanceSecurity(request.Id, featureSecurityDtos);
-        return Ok();
+            new() { Code = "read", Name = "Read", Description = "Can view this feature", Roles = ["admin"] },
+            new() { Code = "edit", Name = "Edit", Description = "Can edit data", Roles = ["admin"] },
+            new() { Code = "delete", Name = "Delete", Description = "Can delete edit data", Roles = ["admin"] },
+        };
     }
 }
