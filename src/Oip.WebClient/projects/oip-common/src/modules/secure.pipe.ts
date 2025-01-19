24a9288038b20fbe9445c2738b14e08ac224e9ce
Igor Tyulyakov <igor.tyulyakov@indusoft.ru>
2025-01-19 12:49:32 +0300

    Add security pipe

---
+++ src/Oip.WebClient/projects/oip-common/src/modules/secure.pipe.ts
@@ -0,0 +1,18 @@
+import {inject, Pipe, PipeTransform} from "@angular/core";
+import {HttpClient} from "@angular/common/http";
+import {DomSanitizer, SafeUrl} from "@angular/platform-browser";
+import {map, Observable} from "rxjs";
+
+@Pipe({
+  standalone: true,
+  name: 'secure'
+})
+export class SecurePipe implements PipeTransform {
+  private readonly http = inject(HttpClient);
+  private readonly sanitizer = inject(DomSanitizer);
+
+  transform(url: any): Observable<SafeUrl> {
+    return this.http
+      .get(url, {responseType: 'blob'}).pipe(map(val => this.sanitizer.bypassSecurityTrustUrl(URL.createObjectURL(val))));
+  }
+}
