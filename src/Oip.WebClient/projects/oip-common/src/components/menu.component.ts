f4ae8eb307a4a2003c408aeeeb5f25b9b12bf16f
Igor Tyulyakov <igor.tyulyakov@indusoft.ru>
2025-02-08 17:48:55 +0300

    add i18n

--- src/Oip.WebClient/projects/oip-common/src/components/menu.component.ts
+++ src/Oip.WebClient/projects/oip-common/src/components/menu.component.ts
@@ -13,36 +13,44 @@ import { InputSwitchModule } from 'primeng/inputswitch';
 import { FormsModule } from "@angular/forms";
 import { MenuItemComponent } from './menu-item.component';
 import { CreateMenuItemDialogComponent } from "./create-menu-item-dialog.component";
+import { TranslatePipe, TranslateService } from "@ngx-translate/core";
 
 
 @Component({
   selector: 'app-menu',
-  template: `<div #emtpty class="layout-sidebar">
-    <ul class="layout-menu">
-      <ng-container *ngFor="let item of model; let i = index;">
-        <li app-menuitem *ngIf="!item.separator" [item]="item" [index]="i" [root]="true" ></li>
-        <li *ngIf="item.separator" class="menu-separator"></li>
-      </ng-container>
-      <div *ngIf="securityService.isAdmin" class="flex items-center absolute right-0 bottom-0 m-4">
-        <label for="adminMode" class="mr-2">All</label>
-        <p-inputSwitch id="adminMode" [(ngModel)]="adminMode" (onChange)="onSettingButtonClick()"></p-inputSwitch>
-      </div>
-      <p-contextMenu [target]="emtpty" [model]="emptySpaceMenu"/>
-    </ul>
-  </div>
-  <create-menu-item-dialog/>`,
+  template: `
+    <div #emtpty class="layout-sidebar">
+      <ul class="layout-menu">
+        <ng-container *ngFor="let item of model; let i = index;">
+          <li app-menuitem *ngIf="!item.separator" [item]="item" [index]="i" [root]="true"></li>
+          <li *ngIf="item.separator" class="menu-separator"></li>
+        </ng-container>
+        <div *ngIf="securityService.isAdmin" class="flex items-center absolute right-0 bottom-0 m-4">
+          <label for="adminMode" class="mr-2">{{ 'menuComponent.all' | translate }}</label>
+          <p-inputSwitch id="adminMode" [(ngModel)]="adminMode" (onChange)="onSettingButtonClick()"></p-inputSwitch>
+        </div>
+        <p-contextMenu [target]="emtpty" [model]="emptySpaceMenu"/>
+      </ul>
+    </div>
+    <create-menu-item-dialog/>`,
   standalone: true,
-  imports: [NgFor, NgIf, MenuItemComponent, ButtonModule, ContextMenuModule, DialogModule, InputTextModule, CreateMenuItemDialogComponent, InputSwitchModule, FormsModule]
+  imports: [NgFor, NgIf, MenuItemComponent, ButtonModule, ContextMenuModule, DialogModule, InputTextModule, CreateMenuItemDialogComponent, InputSwitchModule, FormsModule, TranslatePipe]
 })
 export class MenuComponent implements OnInit {
   readonly menuService = inject(MenuService);
   readonly layoutService = inject(LayoutService);
   readonly securityService = inject(SecurityService);
   readonly msgService = inject(MsgService);
+  readonly translateService = inject(TranslateService);
+
   @ViewChild(CreateMenuItemDialogComponent) createMenuItemDialogComponent: CreateMenuItemDialogComponent;
   model: any[] = [];
   emptySpaceMenu: MenuItem[] = [
-    { label: 'New', icon: PrimeIcons.PLUS, command: (event) => this.newClick(event) },
+    {
+      label: this.translateService.instant('menuComponent.new'),
+      icon: PrimeIcons.PLUS,
+      command: (event) => this.newClick(event)
+    },
   ];
   adminMode: boolean = false;
 
