22463989a07efca1be14782d78b780f806f2f66c
Igor Tyulyakov <g101k@mail.ru>
2025-02-16 20:23:16 +0300

    105 svg logo & clean (#106)
    * Clean logo.component.ts
    
    * Add favicon.svg
    
    * Change icon
    
    * Clean

--- src/Oip.WebClient/projects/oip/src/app.config.ts
+++ src/Oip.WebClient/projects/oip/src/app.config.ts
@@ -14,11 +14,7 @@ import {
 } from "oip-common";
 import { LocationStrategy, PathLocationStrategy } from "@angular/common";
 import { CountryService } from "./app/demo/service/country.service";
-import { CustomerService } from "./app/demo/service/customer.service";
-import { EventService } from "./app/demo/service/event.service";
 import { IconService } from "./app/demo/service/icon.service";
-import { NodeService } from "./app/demo/service/node.service";
-import { PhotoService } from "./app/demo/service/photo.service";
 import { ProductService } from "./app/demo/service/product.service";
 import { MessageService } from "primeng/api";
 import { TranslateLoader, TranslateModule } from "@ngx-translate/core";
@@ -35,11 +31,7 @@ export const appConfig: ApplicationConfig = {
     { provide: LocationStrategy, useClass: PathLocationStrategy },
     { provide: AbstractSecurityStorage, useClass: SecurityStorageService },
     CountryService,
-    CustomerService,
-    EventService,
     IconService,
-    NodeService,
-    PhotoService,
     ProductService,
     AuthGuardService,
     MessageService,
--- src/Oip.WebClient/projects/oip/src/app/demo/api/customer.ts
+++ src/Oip.WebClient/projects/oip/src/app/demo/api/customer.ts
@@ -7,14 +7,3 @@ export interface Representative {
     name?: string;
     image?: string;
 }
-
-export interface Customer {
-    id?: number;
-    name?: string;
-    country?: Country;
-    company?: string;
-    date?: string;
-    status?: string;
-    activity?: number;
-    representative?: Representative;
-}
