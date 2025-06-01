a282a07038b6214270de2cb9847b9abafa0d33ac
Igor Tyulyakov <g101k@mail.ru>
2025-06-01 21:38:28 +0300

    wip

--- src/Oip.WebClient/projects/oip-rts/src/app/components/tag-management/tag-management-settings.component.ts
+++ src/Oip.WebClient/projects/oip-rts/src/app/components/tag-management/tag-management-settings.component.ts
@@ -11,24 +11,13 @@ export interface WeatherSettingsDto {
 @Component({
   selector: 'tag-management-settings',
   template: `
-    <p-fluid>
-      <div class="flex flex-col md:flex-row gap-8">
-        <div class="md:w-1/2">
-          <div class="card flex flex-col gap-4">
-            <div class="font-semibold text-xl">Weather settings</div>
-            <div class="grid grid-cols-12 gap-4">
-              <label for="dayCount" class="flex items-center col-span-12 mb-2 md:col-span-2 md:mb-0">Day Count</label>
-              <div class="col-span-12 md:col-span-10">
-                <input pInputText [(ngModel)]="settings.dayCount" id="dayCount" type="text"/>
-              </div>
-            </div>
-            <div class="flex justify-end">
-              <p-button label="Save" icon="pi pi-save" (onClick)="saveButtonClick($event)"></p-button>
-            </div>
-          </div>
+    <div class="flex flex-col md:flex-row gap-8">
+      <div class="md:w-1/2">
+        <div class="card flex flex-col gap-4">
+          <div class="font-semibold text-xl">Settings</div>
         </div>
       </div>
-    </p-fluid>
+    </div>
   `,
   standalone: true,
   imports: [
