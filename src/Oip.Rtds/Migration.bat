210225a34387139567ef47b862aa9cec379e25d8
Igor Tyulyakov <g101k@mail.ru>
2025-06-13 19:54:53 +0300

    Add bat file for migration

---
+++ src/Oip.Rtds/Migration.bat
@@ -0,0 +1,3 @@
+set migration_name=ChangeType
+dotnet ef migrations add "%migration_name%" --context RtdsMetaContext --project ./../Oip.Rtds.Data.Postgres --output-dir Migrations -- --ConnectionString="XpoProvider=Postgres;Host=localhost;Port=5432;Database=oip;uid=postgres;pwd=" --UseEfCoreProvider=false
+dotnet ef migrations add "%migration_name%" --context RtdsMetaContext --project ./../Oip.Rtds.Data.SqlServer --output-dir Migrations -- --ConnectionString="XpoProvider=MSSqlServer;Server=localhost;Database=oip;uid=sa;Password=" --UseEfCoreProvider=false
