4582ec1fc15a3bb5a1c02f7fdc560c1b7c1e2c86
Igor Tyulyakov <g101k@mail.ru>
2025-07-16 07:24:13 +0300

    Update component to use app title service (#145)
    * Refactor: Extend BaseModuleComponent and move app title service
    
    *   Refactor BaseModuleComponent to use `AppTitleService` instead of `Title`.
    *   Move title handling to `AppTitleService` for better organization.
    *   Update component to display title.
    
    * Change description

---
+++ src/Oip.WebClient/projects/oip-common/src/services/app-title.service.ts
@@ -0,0 +1,29 @@
+import { inject, Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
+import { Title } from "@angular/platform-browser";
+
+/**
+ * Service to manage the application title.
+ */
+@Injectable({ providedIn: 'root' })
+export class AppTitleService {
+  private readonly title = inject(Title);
+  private titleSubject = new BehaviorSubject<string>('');
+  public title$ = this.titleSubject.asObservable();
+
+  /**
+   * Get the title of the current HTML document.
+   */
+  getTitle(): string {
+    return this.titleSubject.value;
+  }
+
+  /**
+   * Set the title of the current HTML document.
+   * @param newTitle
+   */
+  setTitle(newTitle: string): void {
+    this.title.setTitle(newTitle);
+    this.titleSubject.next(newTitle);
+  }
+}
