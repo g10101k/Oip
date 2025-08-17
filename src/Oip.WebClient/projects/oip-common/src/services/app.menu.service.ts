eb7bb4581333429755031eb274f667d01e4f0538
Igor Tyulyakov <g101k@mail.ru>
2025-08-17 12:54:36 +0300

    feat: Load menu immediately
    Load the menu immediately without waiting for the login event.

--- src/Oip.WebClient/projects/oip-common/src/services/app.menu.service.ts
+++ src/Oip.WebClient/projects/oip-common/src/services/app.menu.service.ts
@@ -24,14 +24,7 @@ export class MenuService extends BaseDataService {
   public adminMode: boolean = false;
 
   async loadMenu() {
-    if (this.securityService.accessToken)
-      this.menu = (this.adminMode) ? await this.menuDataService.menuGetAdminMenu() : await this.menuDataService.menuGet();
-    else {
-      this.securityService.onLogin.pipe(take(1)).subscribe(async x => {
-        this.menu = (this.adminMode) ? await this.menuDataService.menuGetAdminMenu() : await this.menuDataService.menuGet();
-      });
-    }
-
+    this.menu = (this.adminMode) ? await this.menuDataService.menuGetAdminMenu() : await this.menuDataService.menuGet();
   }
 
   /**
