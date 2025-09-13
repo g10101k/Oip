fdabfd80d1aa80677ee25be3eb024f60c1c146ae
Igor Tyulyakov <g101k@mail.ru>
2025-09-13 16:46:49 +0300

    feat: Enhance not found page styling and accessibility
    *   Improve visual styling of elements.
    *   Add an ID to the button for accessibility and testing.
    *   Refactor component template for better readability.

--- src/Oip.WebClient/projects/oip-common/src/components/notfound.component.ts
+++ src/Oip.WebClient/projects/oip-common/src/components/notfound.component.ts
@@ -1,40 +1,31 @@
 import { Component } from '@angular/core';
-import { RouterLink } from "@angular/router";
-import { LogoComponent } from "./logo.component";
-import { Button } from "primeng/button";
-import { AppFloatingConfigurator } from "./app.floatingconfigurator";
+import { RouterLink } from '@angular/router';
+import { LogoComponent } from './logo.component';
+import { Button } from 'primeng/button';
+import { AppFloatingConfigurator } from './app.floatingconfigurator';
 
 @Component({
   selector: 'app-notfound',
-  template: `
-    <app-floating-configurator/>
+  template: ` <app-floating-configurator />
     <div class="flex items-center justify-center min-h-screen overflow-hidden">
       <div class="flex flex-col items-center justify-center">
-
         <div
           style="border-radius: 56px; padding: 0.3rem; background: linear-gradient(180deg, color-mix(in srgb, var(--primary-color), transparent 60%) 10%, var(--surface-ground) 30%)">
-
-          <div class="w-full bg-surface-0 dark:bg-surface-900 py-20 px-8 sm:px-20 flex flex-col items-center"
-               style="border-radius: 53px">
+          <div
+            class="w-full bg-surface-0 dark:bg-surface-900 py-20 px-8 sm:px-20 flex flex-col items-center"
+            style="border-radius: 53px">
             <div class="flex flex-col items-center justify-center">
-              <logo width="96" height="96"></logo>
+              <logo height="96" width="96"></logo>
             </div>
             <span class="text-primary font-bold text-3xl">404</span>
             <h1 class="text-surface-900 dark:text-surface-0 font-bold text-3xl lg:text-5xl mb-2">Not Found</h1>
             <div class="text-surface-600 dark:text-surface-200 mb-8">Requested resource is not available.</div>
-            <p-button label="Go to home" routerLink="/"/>
+            <p-button id="oip-app-notfound-go-to-home-button" label="Go to home" routerLink="/" />
           </div>
         </div>
       </div>
-    </div>`
-  ,
-  imports: [
-    RouterLink,
-    LogoComponent,
-    Button,
-    AppFloatingConfigurator
-  ],
+    </div>`,
+  imports: [RouterLink, LogoComponent, Button, AppFloatingConfigurator],
   standalone: true
 })
-export class NotfoundComponent {
-}
+export class NotfoundComponent {}
