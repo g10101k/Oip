f1d047bbfa8529bba8b804e1e642dce156d97039
Igor Tyulyakov <g101k@mail.ru>
2025-09-13 09:45:41 +0300

    Refactor: Add nullability and default values to AppSettings

--- src/Oip.CodeReview/AppSettings.cs
+++ src/Oip.CodeReview/AppSettings.cs
@@ -1,15 +1,48 @@
 namespace Oip.CodeReview;
 
+/// <summary>
+/// Represents the application settings.
+/// </summary>
 public class AppSettings
 {
-    public string WorkDir { get; set; }
-    public string SourceBranch { get; set; }
-    public OllamaSettings Ollama { get; set; } 
-    public string TargetBranch { get; set; }
+    /// <summary>
+    /// Working directory for the code review process.
+    /// </summary>
+    public string WorkDir { get; set; } = null!;
+
+    /// <summary>
+    /// The source branch for the code review process.
+    /// </summary>
+    public string SourceBranch { get; set; } = null!;
+
+    /// <summary>
+    /// The target branch for code review.
+    /// </summary>
+    public string TargetBranch { get; set; } = "main";
+
+    /// <summary>
+    /// File for diff
+    /// </summary>
+    public string? FilePath { get; set; }
+
+    /// <summary>
+    /// Represents the Ollama settings.
+    /// </summary>
+    public OllamaSettings Ollama { get; set; } = null!;
 }
 
+/// <summary>
+/// Represents the Ollama settings.
+/// </summary>
 public class OllamaSettings
 {
-    public string Url { get; set; }
-    public string Model { get; set; }
+    /// <summary>
+    /// The URL of the Ollama server.
+    /// </summary>
+    public string Url { get; set; } = null!;
+
+    /// <summary>
+    /// The name of the Ollama model to use for code review.
+    /// </summary>
+    public string Model { get; set; } = null!;
 }
