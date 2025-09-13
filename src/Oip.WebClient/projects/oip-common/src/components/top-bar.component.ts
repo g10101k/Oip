99cb1fadad8717d245b3555e078c2c8309d35302
Igor Tyulyakov <g101k@mail.ru>
2025-09-13 16:57:42 +0300

    Refactor: Enhance topbar styling and accessibility
    *   Add IDs to elements for accessibility and testing.
    *   Refactor component template for improved readability.
    *   Improve visual styling of elements.

--- src/Oip.WebClient/projects/oip-common/src/components/top-bar.component.ts
+++ src/Oip.WebClient/projects/oip-common/src/components/top-bar.component.ts
@@ -3,90 +3,122 @@ import { MenuItem } from 'primeng/api';
 import { RouterModule } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { StyleClassModule } from 'primeng/styleclass';
-import { LayoutService } from "../services/app.layout.service";
-import { AppConfigurator } from "./app.configurator";
-import { LogoComponent } from "./logo.component";
-import { SecurityService } from "../services/security.service";
-import { TopBarService } from "../services/top-bar.service";
-import { Tab, TabList, Tabs } from "primeng/tabs";
-import { AvatarModule } from "primeng/avatar";
-import { UserService } from "../services/user.service";
+import { LayoutService } from '../services/app.layout.service';
+import { AppConfigurator } from './app.configurator';
+import { LogoComponent } from './logo.component';
+import { SecurityService } from '../services/security.service';
+import { TopBarService } from '../services/top-bar.service';
+import { Tab, TabList, Tabs } from 'primeng/tabs';
+import { AvatarModule } from 'primeng/avatar';
+import { UserService } from '../services/user.service';
+import { ButtonModule } from 'primeng/button';
 
 @Component({
   selector: 'app-topbar',
   standalone: true,
-  imports: [RouterModule, CommonModule, StyleClassModule, AppConfigurator, LogoComponent, Tabs, TabList, Tab, AvatarModule],
+  imports: [RouterModule, CommonModule, StyleClassModule, AppConfigurator, LogoComponent, Tabs, TabList, Tab, AvatarModule, ButtonModule],
   template: `
     <div class="layout-topbar">
       <div class="layout-topbar-logo-container">
         <button class="layout-menu-button layout-topbar-action" (click)="layoutService.onMenuToggle()">
           <i class="pi pi-bars"></i>
         </button>
-        <a class="layout-topbar-logo" routerLink="">
-          <logo width="36px" height="36px"></logo>
+        <a
+          class="layout-topbar-logo"
+           id="oip-app-topbar-logo-link"
+           routerLink="">
+          <logo class="mr-2" [height]="'36px'" [width]="'36px'"></logo>
           <span>OIP</span>
         </a>
       </div>
+    </div>
 
-      <p-tabs *ngIf="securityService.isAdmin && topBarService.topBarItems.length > 0" class="layout-topbar-tabs ml-2"
-              [(value)]="topBarService.activeId">
-        <p-tablist>
-          @for (tab of topBarService.availableTopBarItems; track tab.id) {
-            <p-tab [value]="tab.id">
-              <i class="pi {{tab.icon}}"></i>
-              <span>&nbsp;{{ tab.caption }}</span>
-            </p-tab>
-          }
-        </p-tablist>
-      </p-tabs>
+    <p-tabs
+      *ngIf="securityService.isAdmin && topBarService.topBarItems.length > 0"
+      class="layout-topbar-tabs ml-2"
+      [(value)]="topBarService.activeId">
+      <p-tablist id="oip-app-topbar-tablist">
+        @for (tab of topBarService.availableTopBarItems; track tab.id) {
+          <p-tab id="oip-app-topbar-tab-{{ tab.id }}" [value]="tab.id">
+            <i class="pi {{ tab.icon }} mr-2"></i>
+            <span>{{ tab.caption }}</span>
+          </p-tab>
+        }
+      </p-tablist>
+    </p-tabs>
 
-      <div class="layout-topbar-actions">
-        <div class="layout-config-menu">
-          <button type="button" class="layout-topbar-action" (click)="toggleDarkMode()">
-            <i
-              [ngClass]="{ 'pi ': true, 'pi-moon': layoutService.isDarkTheme(), 'pi-sun': !layoutService.isDarkTheme() }"></i>
-          </button>
-          <div class="relative">
-            <button
-              class="layout-topbar-action layout-topbar-action-highlight"
-              pStyleClass="@next"
-              enterFromClass="hidden"
-              enterActiveClass="animate-scalein"
-              leaveToClass="hidden"
-              leaveActiveClass="animate-fadeout"
-              [hideOnOutsideClick]="true"
-            >
-              <i class="pi pi-palette"></i>
-            </button>
-            <app-configurator/>
-          </div>
-        </div>
-
-        <button class="layout-topbar-menu-button layout-topbar-action" pStyleClass="@next" enterFromClass="hidden"
-                enterActiveClass="animate-scalein" leaveToClass="hidden" leaveActiveClass="animate-fadeout"
-                [hideOnOutsideClick]="true">
-          <i class="pi pi-ellipsis-v"></i>
-        </button>
-
-        <div class="layout-topbar-menu hidden lg:block">
-          <div class="layout-topbar-menu-content">
-            <button type="button" class="layout-topbar-action" (click)="securityService.logout()"
-                    (keydown)="logoutKeyDown($event)">
-              <i class="pi pi-sign-out"></i>
-              <span>Logout</span>
-            </button>
-            <button class="layout-topbar-action" routerLink="config">
-              <p-avatar class="p-link flex align-items-center"
-                        [image]="userService.photoLoaded ? userService.photo : null"
-                        size="normal"
-                        shape="circle">{{ !userService.photoLoaded ? userService.shortLabel : null }}
-              </p-avatar>
-              <span class="ml-2">Profile</span>
-            </button>
-          </div>
+    <div class="layout-topbar-actions">
+      <div class="layout-config-menu">
+        <p-button
+          class="layout-topbar-action"
+          id="oip-app-topbar-theme-button"
+          severity="secondary"
+          type="button"
+          [rounded]="true"
+          [text]="true"
+          (click)="toggleDarkMode()">
+          <i
+            [ngClass]="{
+              'pi ': true,
+              'pi-moon': layoutService.isDarkTheme(),
+              'pi-sun': !layoutService.isDarkTheme()
+            }"></i>
+        </p-button>
+        <div class="relative">
+          <p-button
+            class="layout-topbar-action layout-topbar-action-highlight"
+            enterActiveClass="animate-scalein"
+            enterFromClass="hidden"
+            id="oip-app-topbar-palette-button"
+            leaveActiveClass="animate-fadeout"
+            leaveToClass="hidden"
+            pStyleClass="@next"
+            [hideOnOutsideClick]="true"
+            [rounded]="true">
+            <i class="pi pi-palette"></i>
+          </p-button>
+          <app-configurator/>
         </div>
       </div>
-    </div>`
+
+      <button
+        class="layout-topbar-menu-button layout-topbar-action"
+        enterActiveClass="animate-scalein"
+        enterFromClass="hidden"
+        id="oip-app-topbar-menu-expand-button"
+        leaveActiveClass="animate-fadeout"
+        leaveToClass="hidden"
+        pStyleClass="@next"
+        [hideOnOutsideClick]="true">
+        <i class="pi pi-ellipsis-v"></i>
+      </button>
+
+      <div class="layout-topbar-menu hidden lg:block">
+        <div class="layout-topbar-menu-content">
+          <button
+            class="layout-topbar-action"
+            id="oip-app-topbar-logout-button"
+            type="button"
+            (click)="securityService.logout()"
+            (keydown)="logoutKeyDown($event)">
+            <i class="pi pi-sign-out"></i>
+            <span>Logout</span>
+          </button>
+          <button class="layout-topbar-action" id="oip-app-topbar-profile-button" routerLink="config">
+            <p-avatar
+              class="p-link flex align-items-center"
+              id="oip-app-topbar-user-avatar"
+              shape="circle"
+              size="normal"
+              [image]="userService.photoLoaded ? userService.photo : null">
+              {{ !userService.photoLoaded ? userService.shortLabel : null }}
+            </p-avatar>
+            <span class="ml-2">Profile</span>
+          </button>
+        </div>
+      </div>
+    </div>
+  `
 })
 export class AppTopbar {
   items!: MenuItem[];
@@ -98,12 +130,15 @@ export class AppTopbar {
   }
 
   toggleDarkMode() {
-    this.layoutService.layoutConfig.update((state) => ({ ...state, darkTheme: !state.darkTheme }));
+    this.layoutService.layoutConfig.update((state) => ({
+      ...state,
+      darkTheme: !state.darkTheme
+    }));
   }
 
   logoutKeyDown($event: KeyboardEvent) {
     if ($event.key === 'Enter') {
-      this.securityService.logout()
+      this.securityService.logout();
     }
   }
 }
