4413cd3696723da0f3d4786efab9907b7d19cd12
Igor Tyulyakov <g101k@mail.ru>
2025-09-13 16:33:53 +0300

    Refactor: Improve menu item edit dialog styling and accessibility
    *   Add IDs to buttons for accessibility and testing purposes.
    *   Refactor component template for better readability.
    *   Improve visual styling of elements.

--- src/Oip.WebClient/projects/oip-common/src/components/menu/menu-item-edit-dialog.component.ts
+++ src/Oip.WebClient/projects/oip-common/src/components/menu/menu-item-edit-dialog.component.ts
@@ -1,61 +1,70 @@
 import { Component, EventEmitter, inject, Input, Output } from '@angular/core';
-import { ButtonModule } from "primeng/button";
-import { DialogModule } from "primeng/dialog";
-import { InputTextModule } from "primeng/inputtext";
-import { DropdownModule } from "primeng/dropdown";
-import { FormsModule } from "@angular/forms";
-import { MenuService, SecurityDataService } from "oip-common";
-import { TranslatePipe } from "@ngx-translate/core";
-import { EditModuleInstanceDto } from "../../dtos/edit-module-instance.dto";
-import { MultiSelectModule } from "primeng/multiselect";
+import { ButtonModule } from 'primeng/button';
+import { DialogModule } from 'primeng/dialog';
+import { InputTextModule } from 'primeng/inputtext';
+import { DropdownModule } from 'primeng/dropdown';
+import { FormsModule } from '@angular/forms';
+import { MenuService, SecurityDataService } from 'oip-common';
+import { TranslatePipe } from '@ngx-translate/core';
+import { EditModuleInstanceDto } from '../../dtos/edit-module-instance.dto';
+import { MultiSelectModule } from 'primeng/multiselect';
 
 @Component({
-  imports: [
-    ButtonModule,
-    DialogModule,
-    InputTextModule,
-    DropdownModule,
-    FormsModule,
-    TranslatePipe,
-    MultiSelectModule
-  ],
+  imports: [ButtonModule, DialogModule, InputTextModule, DropdownModule, FormsModule, TranslatePipe, MultiSelectModule],
   selector: 'menu-item-edit-dialog',
   standalone: true,
   template: `
-    <p-dialog header="{{ 'menuItemEditDialogComponent.header' | translate }}" [modal]="true" [(visible)]="visible"
-              [style]="{ width: '40rem' }">
+    <p-dialog
+      header="{{ 'menuItemEditDialogComponent.header' | translate }}"
+      [modal]="true"
+      [style]="{ width: '40rem' }"
+      [(visible)]="visible">
       <div class="flex items-center gap-4 mb-4 mt-1">
-        <label for="menuLabel"
-               class="font-semibold w-1/3">{{ 'menuItemEditDialogComponent.label' | translate }}</label>
-        <input pInputText id="menuLabel" class="flex-auto" autocomplete="off" [(ngModel)]="item.label"/>
-      </div>
-
-      <div class="flex items-center gap-4 mb-4">
-        <label for="icon" class="font-semibold w-1/3">{{ 'menuItemEditDialogComponent.icon' | translate }}
+        <label class="font-semibold w-1/3" for="oip-menu-item-edit-dialog-menu-input">
+          {{ 'menuItemEditDialogComponent.label' | translate }}
         </label>
-        <i class="{{item.icon}}"></i>
-        <input pInputText id="icon" class="flex-auto" [(ngModel)]="item.icon"/>
+        <input
+          autocomplete="off"
+          class="flex-auto"
+          id="oip-menu-item-edit-dialog-menu-input"
+          pInputText
+          [(ngModel)]="item.label"/>
       </div>
 
       <div class="flex items-center gap-4 mb-4">
-        <label for="security"
-               class="font-semibold w-1/3">{{ 'menuItemEditDialogComponent.security' | translate }} </label>
-        <p-multiSelect [options]="roles"
-                       [maxSelectedLabels]="10"
-                       [(ngModel)]="item.viewRoles"
-                       placeholder="Select roles"
-                       appendTo="body"
-                       class="flex-auto"/>
+        <label class="font-semibold w-1/3" for="oip-menu-item-edit-dialog-icon">
+          {{ 'menuItemEditDialogComponent.icon' | translate }}
+        </label>
+        <i class="{{ item.icon }}"></i>
+        <input class="flex-auto" id="oip-menu-item-edit-dialog-icon" pInputText [(ngModel)]="item.icon"/>
+      </div>
+
+      <div class="flex items-center gap-4 mb-4">
+        <label class="font-semibold w-1/3" for="security">
+          {{ 'menuItemEditDialogComponent.security' | translate }}
+        </label>
+        <p-multiSelect
+          appendTo="body"
+          class="flex-auto"
+          id="oip-menu-item-edit-dialog-roles-multi-select"
+          placeholder="Select roles"
+          [maxSelectedLabels]="10"
+          [options]="roles"
+          [(ngModel)]="item.viewRoles"/>
       </div>
 
       <div class="flex justify-end gap-2">
-        <p-button label="{{ 'menuItemEditDialogComponent.cancel' | translate }}"
-                  severity="secondary"
-                  (click)="changeVisible()"
-                  (keydown)="changeVisible()"/>
-        <p-button label="{{ 'menuItemEditDialogComponent.save' | translate }}"
-                  (click)="save()"
-                  (keydown)="save()"/>
+        <p-button
+          id="oip-menu-item-edit-dialog-cancel-edit-button"
+          label="{{ 'menuItemEditDialogComponent.cancel' | translate }}"
+          severity="secondary"
+          (click)="changeVisible()"
+          (keydown)="changeVisible()"/>
+        <p-button
+          id="oip-menu-item-edit-dialog-save-edit-button"
+          label="{{ 'menuItemEditDialogComponent.save' | translate }}"
+          (click)="save()"
+          (keydown)="save()"/>
       </div>
     </p-dialog>
   `
@@ -69,7 +78,14 @@ export class MenuItemEditDialogComponent {
 
   modules: any[] = [];
   roles: string[] = [];
-  item: EditModuleInstanceDto = { icon: "", label: "", viewRoles: [""], moduleId: 0, moduleInstanceId: 0, parentId: 0 };
+  item: EditModuleInstanceDto = {
+    icon: '',
+    label: '',
+    viewRoles: [''],
+    moduleId: 0,
+    moduleInstanceId: 0,
+    parentId: 0
+  };
 
   changeVisible() {
     this.visible = !this.visible;
@@ -94,11 +110,11 @@ export class MenuItemEditDialogComponent {
       parentId: this.menuService.contextMenuItem?.parentId,
       label: this.menuService.contextMenuItem?.label,
       icon: this.menuService.contextMenuItem?.icon,
-      viewRoles: this.menuService.contextMenuItem?.securities,
+      viewRoles: this.menuService.contextMenuItem?.securities
     };
 
     this.roles = await this.securityDataService.getRealmRoles();
-    this.menuService.getModules().then(data => {
+    this.menuService.getModules().then((data) => {
       this.modules = data;
     });
 
