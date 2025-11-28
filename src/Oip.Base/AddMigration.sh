e52f744ea8bdfb0ede564e82e6b45833d8bd4031
Igor Tyulyakov <g101k@mail.ru>
2025-11-28 14:29:53 +0300

    Refactor: Remove migration scripts.

--- src/Oip.Base/AddMigration.sh
+++
@@ -1,3 +0,0 @@
-migration_name=AddComment
-dotnet ef migrations add "${migration_name}" --verbose --context OipModuleContext --project ./../Oip.Base.Data.Postgres --output-dir Migrations -- --ConnectionString="XpoProvider=Postgres;Host=localhost;Port=5432;Database=oip;uid=postgres;pwd=" --UseEfCoreProvider=false
-dotnet ef migrations add "${migration_name}" --verbose --context OipModuleContext --project ./../Oip.Base.Data.SqlServer --output-dir Migrations -- --ConnectionString="XpoProvider=MSSqlServer;Server=localhost;Database=oip;uid=sa;Password=" --UseEfCoreProvider=false
