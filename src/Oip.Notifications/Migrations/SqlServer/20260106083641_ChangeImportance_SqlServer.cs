c3d8071a79466805a682d37b3c1a4eb1fc0336a6
Igor Tyulyakov <g101k@mail.ru>
2026-01-06 11:53:58 +0300

    Add migration to change column sizes and add notification channel code column for Postgres

---
+++ src/Oip.Notifications/Migrations/SqlServer/20260106083641_ChangeImportance_SqlServer.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Oip.Notifications.Migrations.SqlServer
+{
+    /// <inheritdoc />
+    public partial class ChangeImportance_SqlServer : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Notification_Importance",
+                schema: "notifications",
+                table: "Notification");
+
+            migrationBuilder.DropColumn(
+                name: "Importance",
+                schema: "notifications",
+                table: "Notification");
+
+            migrationBuilder.AddColumn<int>(
+                name: "Importance",
+                schema: "notifications",
+                table: "NotificationUser",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                comment: "Importance level of the notification");
+
+            migrationBuilder.AddColumn<int>(
+                name: "Importance",
+                schema: "notifications",
+                table: "NotificationTemplate",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                comment: "Importance level of the notification");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Importance",
+                schema: "notifications",
+                table: "NotificationUser");
+
+            migrationBuilder.DropColumn(
+                name: "Importance",
+                schema: "notifications",
+                table: "NotificationTemplate");
+
+            migrationBuilder.AddColumn<int>(
+                name: "Importance",
+                schema: "notifications",
+                table: "Notification",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                comment: "Importance level of the notification");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Notification_Importance",
+                schema: "notifications",
+                table: "Notification",
+                column: "Importance");
+        }
+    }
+}
