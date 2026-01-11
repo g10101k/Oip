8052965c3a22cd92539052f2122e6dd6cd382d78
Igor Tyulyakov <g101k@mail.ru>
2026-01-11 10:27:11 +0300

    Refactor Program.cs to use explicit WebApplication builder config

--- src/Oip.Users/Program.cs
+++ src/Oip.Users/Program.cs
@@ -2,7 +2,7 @@ using NLog;
 using NLog.Web;
 using Oip.Base.Extensions;
 using Oip.Base.Runtime;
-using Oip.Notifications;
+using Oip.Base.StartupTasks;
 using Oip.Notifications.Base;
 using Oip.Users.Extensions;
 using Oip.Users.Notifications;
@@ -20,7 +20,18 @@ internal static class Program
         try
         {
             var settings = AppSettings.Initialize(args, false, true);
-            var builder = OipModuleApplication.CreateShellBuilder(settings);
+            var builder = WebApplication.CreateBuilder(settings.AppSettingsOptions.ProgramArguments);
+            builder.AddNlog();
+            builder.AddDefaultHealthChecks();
+            builder.AddDefaultAuthentication(settings);
+            builder.AddOpenApi(settings);
+            builder.Services.AddStartupTask<SwaggerGenerateWebClientStartupTask>();
+            builder.Services.AddStartupRunner();
+            builder.Services.AddSingleton(settings);
+            builder.Services.AddScoped<UserService>();
+            builder.Services.AddCors();
+            builder.AddControllersAndView();
+            builder.AddLocalization();
             builder.Services.AddStartupRunner();
             builder.Services.AddSingleton<BaseNotificationService>();
             builder.Services.AddStartupTask<NotificationStartup>();
@@ -37,7 +48,20 @@ internal static class Program
             builder.Services.AddHttpClient();
             builder.Services.AddSingleton<UserService>();
             builder.Services.AddGrpc();
-            var app = builder.BuildApp(settings);
+            
+            var app = builder.Build();
+            app.AddRequestLocalization();
+            app.AddExceptionHandler();
+            app.MapDefaultEndpoints();
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+            app.UseRouting();
+            app.UseAuthentication();
+            app.UseAuthorization();
+            app.UseCors(options => options.AllowAnyOrigin());
+            app.MapControllerRoute(name: "default", pattern: "{controller}/{action=Index}/{id?}");
+            app.MapOpenApi(settings);
+            app.MapFallbackToFile("index.html");
             app.MapGrpcService<UserService>();
             app.MigrateUserDatabase();
             app.Run();
