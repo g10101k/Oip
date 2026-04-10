f24f867a7e25186fafe641151177181f56123d02
Igor Tyulyakov <g101k@mail.ru>
2026-04-10 16:52:30 +0300

    Fix module initialization error (#221)

--- src/Oip.WebClient/projects/oip/src/app/components/weather-forecast-module/weather-forecast-module.component.ts
+++ src/Oip.WebClient/projects/oip/src/app/components/weather-forecast-module/weather-forecast-module.component.ts
@@ -1,5 +1,5 @@
 import { Component, inject, OnDestroy, OnInit, output, ViewChild } from '@angular/core';
-import { BaseModuleComponent, SecurityComponent } from 'oip-common';
+import { BaseModuleComponent, L10nService, SecurityComponent } from 'oip-common';
 import { WeatherForecastModuleApi } from '../../../api/weather-forecast-module.api';
 import { WeatherForecastResponse, WeatherModuleSettings } from '../../../api/data-contracts';
 import { TagModule } from 'primeng/tag';
@@ -9,6 +9,7 @@ import { Button } from 'primeng/button';
 import { FormsModule } from '@angular/forms';
 import { InputText } from 'primeng/inputtext';
 import { DatePipe } from '@angular/common';
+import { TranslatePipe } from '@ngx-translate/core';
 
 interface WeatherModuleLocalSettings {
   first: number;
@@ -28,24 +29,24 @@ interface WeatherModuleLocalSettings {
             <ng-template let-columns pTemplate="header">
               <tr>
                 <th pSortableColumn="date" scope="col">
-                  Date
-                  <p-sortIcon field="date" />
-                  <p-columnFilter display="menu" field="date" type="date" />
+                  {{ 'weather-forecast-module.content.table.date' | translate }}
+                  <p-sortIcon field="date"/>
+                  <p-columnFilter display="menu" field="date" type="date"/>
                 </th>
                 <th pSortableColumn="temperatureC" scope="col">
-                  Temperature C
-                  <p-sortIcon field="temperatureC" />
-                  <p-columnFilter display="menu" field="temperatureC" type="numeric" />
+                  {{ 'weather-forecast-module.content.table.temperatureC' | translate }}
+                  <p-sortIcon field="temperatureC"/>
+                  <p-columnFilter display="menu" field="temperatureC" type="numeric"/>
                 </th>
                 <th pSortableColumn="temperatureF" scope="col">
-                  Temperature F
-                  <p-sortIcon field="temperatureF" />
-                  <p-columnFilter display="menu" field="temperatureF" type="numeric" />
+                  {{ 'weather-forecast-module.content.table.temperatureF' | translate }}
+                  <p-sortIcon field="temperatureF"/>
+                  <p-columnFilter display="menu" field="temperatureF" type="numeric"/>
                 </th>
                 <th pSortableColumn="summary" scope="col">
-                  Summary
-                  <p-sortIcon field="summary" />
-                  <p-columnFilter display="menu" field="summary" type="text" />
+                  {{ 'weather-forecast-module.content.table.summary' | translate }}
+                  <p-sortIcon field="summary"/>
+                  <p-columnFilter display="menu" field="summary" type="text"/>
                 </th>
               </tr>
             </ng-template>
@@ -66,49 +67,57 @@ interface WeatherModuleLocalSettings {
       <div class="flex flex-col md:flex-row gap-8">
         <div class="md:w-1/2">
           <div class="card flex flex-col gap-4">
-            <div class="font-semibold text-xl">Weather settings</div>
+            <div class="font-semibold text-xl">{{ 'weather-forecast-module.settings.title' | translate }}</div>
             <div class="grid grid-cols-12 gap-4">
-              <label class="flex items-center col-span-12 mb-2 md:col-span-2 md:mb-0" for="dayCount">Day Count</label>
+              <label class="flex items-center col-span-12 mb-2 md:col-span-2 md:mb-0" for="dayCount">
+                {{ 'weather-forecast-module.settings.dayCount' | translate }}
+              </label>
               <div class="col-span-12 md:col-span-10">
-                <input id="dayCount" pInputText type="text" [(ngModel)]="settings.dayCount" />
+                <input id="dayCount" pInputText type="text" [(ngModel)]="settings.dayCount"/>
               </div>
             </div>
             <div class="flex justify-end">
-              <p-button icon="pi pi-save" label="Save" (onClick)="saveSettings(settings)"></p-button>
+              <p-button
+                icon="pi pi-save"
+                [label]="'weather-forecast-module.settings.save' | translate"
+                (onClick)="saveSettings(settings)"></p-button>
             </div>
           </div>
         </div>
       </div>
     } @else if (isSecurity) {
-      <security [controller]="controller" [id]="id" />
+      <security [controller]="controller" [id]="id"/>
     }
   `,
   providers: [WeatherForecastModuleApi],
-  imports: [TableModule, SharedModule, TagModule, SecurityComponent, Button, FormsModule, InputText, DatePipe]
+  imports: [TableModule, SharedModule, TagModule, SecurityComponent, Button, FormsModule, InputText, DatePipe, TranslatePipe]
 })
 export class WeatherForecastModuleComponent
   extends BaseModuleComponent<WeatherModuleSettings, WeatherModuleLocalSettings>
-  implements OnInit, OnDestroy
-{
+  implements OnInit, OnDestroy {
   @ViewChild('table') table!: Table;
   protected readonly dataService = inject(WeatherForecastModuleApi);
+  protected readonly l10nService = inject(L10nService);
   protected data: WeatherForecastResponse[] = [];
 
   constructor() {
     super();
+    this.l10nService.get('weather-forecast-module').subscribe((q) => {
+      this.appTitleService.setTitle(q.title);
+    });
   }
 
-  async ngOnInit() {
-    await super.ngOnInit();
+  protected override async onModuleInstanceChange(): Promise<void> {
     try {
       this.data = await this.dataService.getWeatherForecast({
         dayCount: this.settings.dayCount
       });
     } catch (error) {
+      this.data = [];
       console.error('Error fetching weather data:', error);
       this.msgService.error(error.error.message);
     }
-    if (this.localSettings().filters) this.table.filters = this.localSettings().filters;
+    if (this.localSettings().filters && this.table) this.table.filters = this.localSettings().filters;
   }
 
   onFilter() {
