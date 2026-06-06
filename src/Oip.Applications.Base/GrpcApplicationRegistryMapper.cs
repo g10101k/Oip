93ce6ec63dd54a645830f512a679f734988af404
Igor Tyulyakov <g101k@mail.ru>
2026-06-06 17:40:46 +0300

    To mapperly

--- src/Oip.Applications.Base/GrpcApplicationRegistryMapper.cs
+++ src/Oip.Applications.Base/GrpcApplicationRegistryMapper.cs
@@ -1,40 +1,16 @@
 using Oip.Applications.Base.Contracts;
 using Oip.Applications.Base.Grpc;
+using Riok.Mapperly.Abstractions;
 
 namespace Oip.Applications.Base;
 
 /// <summary>
 /// Maps application registry contracts between DTO and gRPC shapes.
 /// </summary>
-public static class GrpcApplicationRegistryMapper
+[Mapper]
+public static partial class GrpcApplicationRegistryMapper
 {
-    public static ApplicationRegistryItemDto ToDto(this ApplicationRegistryItem application)
-    {
-        return new ApplicationRegistryItemDto
-        {
-            Code = application.Code,
-            DisplayName = application.DisplayName,
-            BaseUrl = application.BaseUrl,
-            ApiBaseUrl = application.ApiBaseUrl,
-            Icon = application.Icon,
-            Order = application.Order,
-            Enabled = application.Enabled,
-            IsCurrent = application.IsCurrent
-        };
-    }
+    public static partial ApplicationRegistryItemDto ToDto(this ApplicationRegistryItem application);
 
-    public static ApplicationRegistryItem ToGrpc(this ApplicationRegistryItemDto application)
-    {
-        return new ApplicationRegistryItem
-        {
-            Code = application.Code,
-            DisplayName = application.DisplayName,
-            BaseUrl = application.BaseUrl,
-            ApiBaseUrl = application.ApiBaseUrl,
-            Icon = application.Icon,
-            Order = application.Order,
-            Enabled = application.Enabled,
-            IsCurrent = application.IsCurrent
-        };
-    }
+    public static partial ApplicationRegistryItem ToGrpc(this ApplicationRegistryItemDto application);
 }
